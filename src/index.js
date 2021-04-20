ba0d28f79ce1f333eb2248225fd0311ef4a52acc
berkode <berkino@gmail.com>
2021-04-20 19:27:37 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const webport = process.env.PORT || 8088;
+
+const dateUp = Date.now();
+
+app.get('/', (req, res) => {
+  const today = new Date();
+
+  res.json({
+    date: today,
+    up: `${(Date.now() - dateUp)/1000} seg.`,
+  });
+});
+
+app.listen(webport, () => {
+  console.log(`Web service running on port: ${webport}`);
+  console.log('Press CTRL + C to quit');
+});
